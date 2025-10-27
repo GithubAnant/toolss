2c35cff4567393aa48d62b370480e706aff7dcab
Anant Singhal <89266869+GithubAnant@users.noreply.github.com>
2025-10-27 23:22:26 +0530

    vite vercel fixes

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://nplmdveyzfilkmggfxbw.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wbG1kdmV5emZpbGttZ2dmeGJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3ODI1MTksImV4cCI6MjA3NjM1ODUxOX0.hAETF2fxQBMLc8y7uC64CpeeX7HZ7fX14UnR7rU-HmU';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
