fb9a8e9fd223941b4617ed45750d271c4f5479be
nishb <nishbcodes@gmail.com>
2025-11-03 23:34:09 +0530

    Categories List moved to contants folder

---
+++ src/constants/categories.ts
@@ -0,0 +1,21 @@
+const categories = [
+  { value: "browsers", label: "browsers" },
+  { value: "ai", label: "ai" },
+  { value: "no code", label: "no code" },
+  { value: "design", label: "design" },
+  { value: "coding", label: "coding" },
+  { value: "video", label: "video" },
+  { value: "tools", label: "tools" },
+  { value: "development tools", label: "development tools" },
+  { value: "social", label: "social" },
+  { value: "finance", label: "finance" },
+  { value: "health", label: "health" },
+  { value: "education", label: "education" },
+  { value: "sports", label: "sports" },
+  { value: "travel", label: "travel" },
+  { value: "food", label: "food" },
+  { value: "music", label: "music" },
+  { value: "gaming", label: "gaming" },
+];
+
+export default categories;
